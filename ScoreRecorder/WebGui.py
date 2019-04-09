8303e4309db472ef936fdf4e554e450468d964dd
MrGodfrey <562568681@qq.com>
2019-04-09 08:50:31 +0800

    add upload file page

--- ScoreRecorder/WebGui.py
+++ ScoreRecorder/WebGui.py
@@ -1,6 +1,11 @@
 from flask import Flask,render_template,request,flash
 from ScoreRecorder import Data
 
+# 上传文件用
+import os
+from flask import redirect,url_for
+from werkzeug.utils import secure_filename
+
 # request can deal post.
 # flash 可以动态传递消息 --> 需要对内容加密，设置scret_key
 
@@ -38,6 +43,40 @@ def index():
             return render_template('index.html',person=p,scores=showScores(p))
         
     return render_template('index.html',person=None,scores=None)
+
+# 准备上传文件
+# UPLOAD_FOLDER='/home/ubuntu/project/scoreFlask'
+UPLOAD_FOLDER='C:\\Users\\56256\\Documents\\GitHub\\Tools\\ScoreRecorder'
+ALLOWED_FILE=set(['Data.bak','Data.dat','Data.dir'])
+app.config['UPLOAD_FOLDER']=UPLOAD_FOLDER
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file.filename in ALLOWED_FILE:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(request.url)
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
 
 if __name__=="__main__":
     app.config['DEBUG']=True
